cf4ed55bc80a3f664d91e66031339357bffa0968
YaChun07 <yachun07@gmail.com>
2017-09-03 18:15:14 +0800

    Use two for-loop to find two sum position. O(n^2)

---
+++ HWTwoSum/TwoSum/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class TwoSum
+    {
+        public static int[] twoSum(int[] inputArray, int target)
+        {
+            int[] twoSumList = new int[2];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] + inputArray[j] == target)
+                    {
+                        twoSumList[0] = i;
+                        twoSumList[1] = j;
+                    }
+                }
+            }
+            return twoSumList;
+        }
+    }
+}
---
+++ HWTwoSum/TwoSumTests/TwoSumTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TwoSum.Tests
+{
+    [TestClass()]
+    public class TwoSumTests
+    {
+        [TestMethod()]
+        public void four_positive_number()
+        {
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, TwoSum.twoSum(new int[] { 2, 7, 11, 15 }, 9));
+        }
+        [TestMethod()]
+        public void two_positive_number_two_negative_number()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3 }, TwoSum.twoSum(new int[] { 2, 7, -11, -15 }, -8));
+        }
+        [TestMethod()]
+        public void two_positive_number_one_negative_number_one_zero_()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 3 }, TwoSum.twoSum(new int[] { 2, 7, -11, 0 }, -11));
+        }
+    }
+}
