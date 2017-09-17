7ab91a0c358b81cfe36c4e3aa68425d260f1ec5e
YaChun07 <yachun07@gmail.com>
2017-09-17 23:56:36 +0800

    Use extensible twoSum algorithm with a loop

---
+++ ThreeSum/ThreeSum/threeSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSum
+{
+    public class threeSum
+    {
+        public static List<List<int>> threeSumRef(int[] nums)
+        {
+            Array.Sort(nums);
+
+            List<List<int>> answers = new List<List<int>>();
+            if (nums.Length < 3)
+            {
+                return answers;
+            }
+            for (int count = 0; count < nums.Length - 2; count++)
+            {
+                int target = 0 - nums[count];
+
+                if (target < nums[count + 1])
+                {
+                    break;
+                }
+
+                for (int k = count + 1; k < nums.Length; k++)
+                {
+
+                    if (nums.Contains(target - nums[k]) && Array.LastIndexOf(nums, target - nums[k]) > k)
+                    {
+
+                        answers.Add(new List<int> { nums[count], nums[k], target - nums[k] });
+
+                        while (k < nums.Length - 1 && nums[k] == nums[k + 1])
+                        {
+                            k++;
+                        }
+                    }
+                }
+                while (count < nums.Length - 1 && nums[count] == nums[count + 1])
+                {
+                    count++;
+                }
+            }
+            return answers;
+        }
+    }
+
+}
---
+++ ThreeSum/ThreeSumTests/threeSumTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreeSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSum.Tests
+{
+    [TestClass()]
+    public class threeSumTests
+    {
+        [TestMethod()]
+        public void TwoAnswerTest()
+        {
+            var threeSumList = new List<List<int>>()
+            {
+
+                new List<int>() { -1, -1, 2 },
+                new List<int>() { -1, 0, 1 }
+
+            };
+
+            for (int i = 0; i < threeSumList.Count; i++)
+            {
+                CollectionAssert.AreEqual(threeSumList[i], threeSum.threeSumRef(new int[] { -1, 0, 1, 2, -1, -4 })[i]);
+            }
+        }
+        [TestMethod()]
+        public void SingleAnswerTest()
+        {
+            var threeSumList = new List<List<int>>()
+            {
+                new List<int>() { -3,0,3 }
+
+            };
+            for (int i = 0; i < threeSumList.Count; i++)
+            {
+                CollectionAssert.AreEqual(threeSumList[i], threeSum.threeSumRef(new int[] { 2, 3, -3, 6, 8, 9, 0 })[i]);
+
+            }
+        }
+        [TestMethod()]
+        public void ZeroAnswerTest()
+        {
+            var threeSumList = new List<List<int>>()
+            {
+                new List<int>() { 0, 0, 0 }
+
+            };
+
+            for (int i = 0; i < threeSumList.Count; i++)
+            {
+                CollectionAssert.AreEqual(threeSumList[i], threeSum.threeSumRef(new int[] { 0, 0, 0, 0, 0, 0 })[i]);
+            }
+        }
+        [TestMethod()]
+        public void DuplicateAnswerTest()
+        {
+            var threeSumList = new List<List<int>>()
+            {
+                new List<int>() { -1, 0, 1 }
+
+            };
+
+            for (int i = 0; i < threeSumList.Count; i++)
+            {
+                CollectionAssert.AreEqual(threeSumList[i], threeSum.threeSumRef(new int[] { -1, 0, 1, -1, 0, 1 })[i]);
+            }
+        }
+
+
+    }
+}
